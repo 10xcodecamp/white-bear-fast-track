cedce56bf222854e0cb86386d25a39fe4a9b4e4e
149203 <149203@gmail.com>
2020-09-03 17:02:28 -0700

    introducing strings

--- script.js
+++ script.js
@@ -2,28 +2,18 @@ $("#save-card").click(function () {
    $("#overlay-success").toggleClass("d-flex d-none");
 });
 
-let imageryCharsCount = 0;
+$("#create-imagery-input").keyup(function (e) {
+   console.log("Event: ", e);
 
-$("#create-imagery-input").keydown(function (e) {
-   const key = e.which;
-   console.log("Key inputted: ", key);
+   // get the text from the textarea
+   const text = e.target.value;
+   const username = `Mike`;
+   console.log(`${username} inputted: ${text}`);
 
-   // If the key the user has pressed is backspace, decrement the count
-   if (key === 8) {
-      console.log("The user has pressed backspace!");
-      imageryCharsCount--;
-      if (imageryCharsCount < 0) {
-         console.log("You have entered negative territory!");
-         imageryCharsCount = 0;
-      }
-   } else if (key === 16 || 18 || 46 || 35 || 36 || 20) {
-      console.log("This doesn't count in the character counter.");
-   } else {
-      //Else, increment the count
-      console.log("The user has pressed any other key!");
-      imageryCharsCount = imageryCharsCount + 1;
-   }
+   // check the length of the text
+   const textLength = text.length;
+   console.log(`Today's total inputted chars: ${textLength}`);
 
-   console.log("Total inputted chars: ", imageryCharsCount);
-   $("#imagery-char-count").html(imageryCharsCount);
+   // update the character counter on the page
+   $("#imagery-char-count").html(textLength);
 });
